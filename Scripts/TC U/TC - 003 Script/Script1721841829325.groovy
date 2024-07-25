089185d378533a93cce36ed709933834d61ff92a
scarl <scarl@Sanchita.lan>
2024-07-25 14:19:25 +0200

    Profiles

--- Scripts/TC U/TC - 003 Script/Script1721841829325.groovy
+++ Scripts/TC U/TC - 003 Script/Script1721841829325.groovy
@@ -16,10 +16,18 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
-WebUI.openBrowser("")
-WebUI.navigateToUrl("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-WebUI.setText(findTestObject('Object Repository/Udemy Orange HRM/Page_OrangeHRM/input_Username_username'), "Admin")
-WebUI.setEncryptedText(findTestObject('Object Repository/Udemy Orange HRM/Page_OrangeHRM/input_Password_password'), "hUKwJTbofgPU9eVlw/CnDQ==")
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+WebUI.setText(findTestObject('Object Repository/Udemy Orange HRM/Page_OrangeHRM/input_Username_username'), 'Admin')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Udemy Orange HRM/Page_OrangeHRM/input_Password_password'), 'hUKwJTbofgPU9eVlw/CnDQ==')
+
 WebUI.click(findTestObject('Object Repository/Udemy Orange HRM/Page_OrangeHRM/button_Login'))
-WebUI.verifyTextPresent("Dashboard", false)
+
+WebUI.verifyTextPresent('Dashboard', false)
+
 WebUI.closeBrowser()
+

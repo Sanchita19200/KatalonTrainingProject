92c5909b72922628cbff7ee90473ad11f8e56d64
scarl <scarl@Sanchita>
2024-07-29 23:06:57 +0200

    TD Excel

---
+++ Test Suites/DataDrivenTesting.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DataDrivenTesting</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>dab16125-8487-442b-83d1-ac2c991e84f8</testSuiteGuid>
+   <testCaseLink>
+      <guid>1ee33172-cf15-41dd-b100-86bf036b9811</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC UDEMY/DataDrivenTesting</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>caf8886d-5ebb-4519-a718-dcf67006e67b</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/LoginData</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>caf8886d-5ebb-4519-a718-dcf67006e67b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>ba4f97cb-afd5-4632-a67b-30e1a37622ef</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>caf8886d-5ebb-4519-a718-dcf67006e67b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>ef099403-4d45-4a6f-81b6-3c39e68f5b71</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
